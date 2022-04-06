3683f1979ac215795d6fb0ae6a3518a5f7a88ed7
Ella Juengst <ellaj@live.unc.edu>
2022-04-06 14:03:36 -0400

    adding tag nav functionality

--- src/pages/ResourceLibrary.js
+++ src/pages/ResourceLibrary.js
@@ -12,14 +12,18 @@ import CategoryNav from "../components/CategoryNav";
 import TagNav from "../components/TagNav";
 import SearchBar from "../components/SearchBar"
 import AddResource from "../components/AddResourceButton"
+import {ReactComponent as ArrowDown} from '../assets/arrow-down.svg'
+import {ReactComponent as X} from '../assets/x.svg'
 
 export default function ResourceLibrary() {
   const {currentUser} = useAuth();
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const [resources, setResources] = useState([]);
-  const ref = firebase.firestore().collection("resources");
-  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [tags, setTags] = useState([]);
+
+  const ref = firebase.firestore();
+  const [selectedTags, setSelectedTags] = useState([]);
 
   function navigateToSignIn() {
     navigate("/login");
@@ -27,7 +31,8 @@ export default function ResourceLibrary() {
 
   function getResources() {
     setLoading(true);
-    ref.onSnapshot((querySnapshot) => {
+    const resourcesRef = ref.collection("resources")
+    resourcesRef.onSnapshot((querySnapshot) => {
       const items = [];
       querySnapshot.forEach((doc) => {
         items.push(doc.data());
@@ -37,8 +42,33 @@ export default function ResourceLibrary() {
     });
   }
 
+  function getTags() {
+    const tagsRef = ref.collection("tags")
+    tagsRef.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setTags(items);
+    });
+  }
+
+  function selectTag(e) {
+    const tag = e.target.innerHTML;   
+    const index = selectedTags.indexOf(tag);
+    if (index !== -1) {
+      setSelectedTags(selectedTags.filter((e) => e !== tag));
+      e.target.classList.remove('active-link');
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+      e.target.classList.add('active-link');
+    }
+  }
+
   useEffect(() => {
     getResources();
+    getTags();
+
   }, []);
   
   return ( 
@@ -61,7 +91,24 @@ export default function ResourceLibrary() {
         <Container fluid={true}>
             <Row>
                 <Col className="tag-nav-container" lg={2}>
-                    <TagNav />
+                  <Navbar className="flex-column sidebar" expand="lg">
+                    <Navbar.Brand className="filters-brand">Filters</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="filter-sidebar-nav"><div className="arrow-down"><ArrowDown /></div></Navbar.Toggle>
+                    <Navbar.Collapse id="filter-sidebar-nav">
+                    <Nav className="bg-light flex-column sidebar"
+                    // activeKey={"/home"}
+                    // onSelect={selectedKey => console.log(selectedKey)}
+                    >
+                        {tags.map((tag) => {
+                          return (
+                            <Nav.Item className="sidebar-item">
+                              <Nav.Link className="sidebar-link" onClick={selectTag}>{tag.name}</Nav.Link>
+                            </Nav.Item>
+                          )
+                        })}
+                    </Nav>
+                    </Navbar.Collapse>
+                  </Navbar>
                 </Col>
                 <Col>
                     <div className="search-bar-container">
@@ -69,6 +116,16 @@ export default function ResourceLibrary() {
                       {currentUser && 
                       <AddResource />
                       }
+                      
+                      
+                     
+                    </div>
+                    <div className="tags-container">
+                    {selectedTags.map((tag) => {
+                        return (
+                        <div className="tag-pill" key={tag}>{tag}<div className="x"><X /></div></div>
+                        )
+                      })}
                     </div>
                     
                     <Container className="resources-container">
